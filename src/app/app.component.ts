61804299d40ce5dcf89d2e8b52a14eac529abb1f
badal sarkar <badalsarkar86@gmail.com>
2020-06-13 00:18:19 -0400

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 /*********************************************************************************
-* WEB422 – Assignment 05
+* WEB422 – Assignment 06
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or
 * distributed to other students.
 *
 * Name: Badal Sarkar     Student ID:137226189    Date: March 27, 2020
+* Online Link: https://5e91041b396d246b42f682a2--tender-kowalevski-b9e95e.netlify.com/home
 *
 ********************************************************************************/ 
 
